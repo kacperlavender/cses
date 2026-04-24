d5ec16bb22c243c1c8bd9ac8d011d9f399486197
kacper <kacper.kielbasa5000@gmail.com>
2026-04-24 10:51:13 +0200

    skończenie 004

--- 004/increasing_array.py
+++ 004/increasing_array.py
@@ -6,9 +6,9 @@ def solve():
 
     for i in range(1, len(l)):
         if l[i] < l[i - 1]:
-            while l[i] < l[i - 1]:
-                l[i] = l[i] + 1
-                amount += 1
+            diff = abs(l[i] - l[i - 1])
+            l[i] += diff
+            amount += diff
         
     print(amount)
     
